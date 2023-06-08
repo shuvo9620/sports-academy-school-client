0cce6e3ed07557889dc5650d78939e8ec3a87c49
shuvo9620 <smahfuzur0@gmail.com>
2023-06-08 18:46:42 +0600

    see conditional login button and image in navbar

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,8 @@ import {
 } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
+import Login from "../pages/Login/Login";
+import SignUp from "../pages/SignUp/SignUp";
 
 export const router = createBrowserRouter([
     {
@@ -12,6 +14,14 @@ export const router = createBrowserRouter([
             {
                 path: "/",
                 element: <Home></Home>,
+            },
+            {
+                path: 'login',
+                element: <Login></Login>,
+            },
+            {
+                path: 'signup',
+                element: <SignUp></SignUp>
             }
         ]
     },
--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../../providers/AuthProvider";
+import { useContext } from "react";
 
 const Navbar = () => {
+  const { user, logOut } = useContext(AuthContext);
+  const handleLogOut = () => {
+    logOut().then(() => { }).catch(error => console.error(error));
+  };
+
   return (
     <nav className="bg-gray-800 py-4">
       <div className="container mx-auto flex items-center justify-between px-4">
@@ -7,15 +15,39 @@ const Navbar = () => {
           <img src="logo.png" alt="Logo" className="h-8 w-auto" />
           <span className="text-white text-lg font-semibold ml-2">Sports Academy</span>
         </div>
-        <div className="flex items-center justify-center flex-1">
-          <a href="/" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded">
+        <div>
+          <Link to='/' className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded">
             Home
-          </a>
-          <a href="/about" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded">
+          </Link>
+          <Link to='about' className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded">
             About
-          </a>
+          </Link>
         </div>
-        <img src="/path/to/user-photo.png" alt="User" className="h-8 w-8 rounded-full ml-4" />
+
+        <div className="flex items-center ml-4">
+          {user ? (
+            <>
+              <img
+                className="h-8 w-8 rounded-full"
+                src={user.photoURL}
+                title={user.displayName}
+              />
+              <button
+                onClick={handleLogOut}
+                className="ml-4 px-4 py-2 rounded bg-gray-800 text-white hover:bg-gray-600"
+              >
+                Log Out
+              </button>
+            </>
+          ) : (
+            <Link to="/login">
+              <button className="px-4 py-2 rounded bg-blue-600 text-white hover:bg-blue-500">
+                Login
+              </button>
+            </Link>
+          )}
+        </div>
+
       </div>
     </nav>
   );
