0f81e5c2b55b268585a106e661e5e87e8d5340d9
shuvo9620 <smahfuzur0@gmail.com>
2023-06-12 22:37:49 +0600

    extra section resolved

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,16 +1,14 @@
 
 import Banner from '../Banner/Banner';
-import JoinTeam from './UpcomingEvents';
-import PopularInstructor from './PopularInstructor';
 import PopularClasses from './PopularClasses';
+import UpcomingEvents from './UpcomingEvents';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            <PopularInstructor></PopularInstructor>
             <PopularClasses></PopularClasses>
-            <JoinTeam></JoinTeam>
+            <UpcomingEvents></UpcomingEvents>
         </div>
     );
 };
--- src/pages/Home/Home/UpcomingEvents.jsx
+++ src/pages/Home/Home/UpcomingEvents.jsx
@@ -23,12 +23,9 @@ const UpcomingEvents = () => {
 
     return (
         <div className="bg-gray-100">
-            {/* Other homepage content */}
-
-            {/* Upcoming Events Section */}
             <section className="py-8">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <h2 className="text-2xl font-bold mb-4">Upcoming Events</h2>
+                    <h2 className="text-3xl text-center font-bold mb-4">Upcoming Events</h2>
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                         {upcomingEvents.map((event, index) => (
                             <div key={index} className="bg-white p-4 rounded-md">
@@ -37,7 +34,7 @@ const UpcomingEvents = () => {
                                 <p className="text-gray-600 mb-2">{event.date}</p>
                                 <p className="text-gray-800 mb-2">{event.location}</p>
                                 {/* Add a button or link to redirect to the event's detailed page */}
-                                <button className="bg-blue-500 text-white rounded-md px-4 py-2">Learn More</button>
+                                <button className="btn btn-primary text-white rounded-md px-4 py-2">Learn More</button>
                             </div>
                         ))}
                     </div>
